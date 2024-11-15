6d11698b8bbc03683a505073506c4673a29c2b84
Angel Julio Retamozo Calatayud <aretamozoc@unsa.edu.pe>
2024-11-14 19:27:31 -0500

    pregunta1.pl
    Intento de acceso a una base de datos y modificarla, me parece que el html y el perl respectivo deben de modificarse

---
+++ pregunta1.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use CGI qw(:standard);
+use DBI;
+
+# Configuración de conexión
+my $database = "prueba";
+my $hostname = "mariadb2";         # Nombre del contenedor MariaDB
+my $port     = 3307;               # Puerto predeterminado de MariaDB
+my $user     = "root";             # Usuario de MariaDB
+my $password = "tu_contraseña_segura"; # Contraseña para el usuario root
+
+# DSN de conexión
+my $dsn = "DBI:mysql:database=$database;host=$hostname;port=$port";
+
+# Crear un nuevo objeto CGI
+my $query = CGI->new();
+
+# Recoger los datos del formulario 
+my $campo = $query->param('campo'); 
+my $propiedad = $query->param('propiedad'); 
+my $dato = $query->param('dato'); 
+# Conectar a la base de datos 
+my $dbh = DBI->connect($dsn, $user, $password, {
+     RaiseError => 1, PrintError => 0, mysql_enable_utf8 => 1, 
+     }); 
+print $query->header(-type => 'text/html', -charset => 'utf-8');
+print $query->start_html('Actualizar Datos'); 
+if (!$dbh) {
+    die "Error al conectar a la base de datos: $DBI::errstr\n";
+}
+    # Construir la consulta SQL 
+    my $sql = "UPDATE $campo SET $propiedad = ?"; 
+    my $sth = $dbh->prepare($sql);
+    if ($sth->execute($dato)) {
+        print "<h2>Registro actualizado correctamente</h2>"; 
+    } else { 
+        print "<h2>Error al actualizar el registro</h2>";
+    } 
+    # Finalizar HTML 
+print $query->end_html;
+  # Cerrar la conexión 
+$sth->finish(); $dbh->disconnect(); 
